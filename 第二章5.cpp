8ef01ec382d718a76a625332f355bdc077ef21b0
MZ66666 <3573442755@qq.com>
2019-11-26 23:38:45 +0800

    练习

---
+++ 第二章5.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int years,days;
+	years=18;
+	days=18*365;
+	printf("%d years is %d days\n",years,days);
+	return 0;
+}
---
+++ 第二章6.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int f,i;
+	i=0;
+	while(f<3)
+	{
+	f=i++;
+	printf("For he is jolly good fellow!\n"); 
+}
+ 	printf("which nobody can deny\n");
+ 	return 0;
+ }
